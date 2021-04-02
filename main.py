2f55ae9ff06b8b3aa86323600c56cf666b775070
выауу <camgal04@gmail.com>
2021-04-02 17:16:39 +0500

    Часть №2

--- main.py
+++ main.py
@@ -3,30 +3,32 @@ import sys
 from io import BytesIO
 
 import requests
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 SCREEN_SIZE = 600, 450
 
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.scale = 17  # текущий масштаб
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
-        self.pixmap = QPixmap.fromImage(QImage.fromData(self.get_image()))
         self.image = QLabel(self)
+        self.pixmap = None
         self.image.move(0, 0)
         self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+        self.update_pixmap()
 
     def get_image(self) -> bytes:
         """Возвращает изображение (карту)"""
         map_params = {
             "ll": "37.530887,55.703118",
             "l": "map",
-            "z": "17"
+            "z": self.scale
         }
 
         map_api_server = "http://static-maps.yandex.ru/1.x/"
@@ -36,6 +38,20 @@ class Window(QWidget):
                   f"({response.reason})")
             sys.exit(1)
         return response.content
+
+    def update_pixmap(self) -> None:
+        """Обновляет изображение (карту)"""
+        self.pixmap = QPixmap.fromImage(QImage.fromData(self.get_image()))
+        self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        # изменение масштаба
+        if event.key() == Qt.Key_PageUp:
+            self.scale = min(self.scale + 1, 17)
+            self.update_pixmap()
+        elif event.key() == Qt.Key_PageDown:
+            self.scale = max(self.scale - 1, 0)
+            self.update_pixmap()
 
 
 if __name__ == '__main__':

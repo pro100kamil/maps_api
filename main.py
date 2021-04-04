a40f4e4232d0e0c22b87420a346351a593748b22
выауу <camgal04@gmail.com>
2021-04-04 21:35:55 +0500

    Часть №12

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import os
 import sys
 from io import BytesIO
@@ -12,6 +13,23 @@ from PyQt5.uic import loadUi
 WIDTH, HEIGHT = MAP_SIZE = 600, 450
 
 
+def lonlat_distance(a: str, b: str) -> float:
+    """Расстояние между точками"""
+    degree_to_meters_factor = 111 * 1000  # 111 километров в метрах
+    a_lon, a_lat = map(float, a.split(','))
+    b_lon, b_lat = map(float, b.split(','))
+
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    # Вычисляем смещения в метрах по вертикали и горизонтали.
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    return round(math.sqrt(dx * dx + dy * dy), 4)
+
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -19,17 +37,18 @@ class Window(QMainWindow):
         loadUi('main.ui', self)
 
         self.lon, self.lat = 37.530887, 55.703118  # долгота и широта
-        self.scale = 14  # текущий масштаб
+        self.scale = 17  # текущий масштаб
         self.map_type = 'map'  # тип карты
         self.pt = None  # текущая метка
-        self.address = None
-        self.index = None
+        self.address = 'Россия, Москва, Западный административный округ, ' \
+                       'район Раменки, микрорайон Ленинские Горы, 1'
         self.map_file = "map.png"  # файл с картой
 
         self.pixmap = None
 
         self.types_of_map.buttonToggled.connect(self.get_type_of_map)
-        self.search_btn.clicked.connect(lambda text: self.search_toponym(geocode=self.input_toponym.text()))
+        self.search_btn.clicked.connect(lambda text: self.search_toponym(
+            geocode=self.input_toponym.text()))
         self.reset_btn.clicked.connect(self.reset_search)
         self.post_code.stateChanged.connect(self.change_state_post_code)
 
@@ -68,8 +87,8 @@ class Window(QMainWindow):
 
         return response.content
 
-    def search_toponym(self, geocode, mode=1) -> None:
-        """Поиск топонима по нажатию кнопки поиска"""
+    def search_toponym(self, geocode: str, change_pt=True) -> None:
+        """Поиск топонима"""
 
         geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
@@ -99,13 +118,13 @@ class Window(QMainWindow):
 
         toponym = variants[0]
 
-        if mode:
+        if change_pt:
             self.lon, self.lat = tuple(
                 map(float, toponym["GeoObject"]["Point"]["pos"].split()))
             self.pt = f"{self.lon},{self.lat},pm2rdl"
+
         self.address = toponym['GeoObject']['metaDataProperty'][
             'GeocoderMetaData']['text']
-
         post_code = toponym['GeoObject']['metaDataProperty'][
             "GeocoderMetaData"]["Address"].get("postal_code")
 
@@ -118,6 +137,40 @@ class Window(QMainWindow):
 
         self.update_pixmap()
 
+    def search_organization(self, coords: str) -> None:
+        """Возращает организацию"""
+        search_api_server = "https://search-maps.yandex.ru/v1/"
+
+        search_params = {
+            "apikey": "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3",
+            "lang": "ru_RU",
+            "text": "магазин",
+            "ll": coords,
+            "type": "biz"
+        }
+
+        response = requests.get(search_api_server, params=search_params)
+
+        variants = response.json()["features"]
+
+        if not variants:
+            print('Организации не найдено')
+            return
+
+        org = variants[0]
+        coords_org = ','.join(tuple(map(str, org["geometry"]["coordinates"])))
+
+        if lonlat_distance(coords, coords_org) <= 50:
+            name = org["properties"]["CompanyMetaData"]["name"]
+            self.pt = coords_org + ',pm2rdl'
+            self.show_message(
+                msg=name,
+                style='color: white; background-color: green; '
+                      'font-size: 10pt; text-align: center;')
+            self.update_pixmap()
+        else:
+            print('Близкой организации не найдено')
+
     def update_pixmap(self) -> None:
         """Обновляет изображение (карту)"""
 
@@ -127,17 +180,30 @@ class Window(QMainWindow):
         self.image.setPixmap(self.pixmap)
         self.image.setFocus()
 
-    def change_state_post_code(self):
+    def change_state_post_code(self) -> None:
         """Обработка нажатия на checkbox"""
 
-        if self.address is not None:
-            self.search_toponym(self.address, mode=0)
+        if self.pt is not None:  # если стоит метка
+            self.search_toponym(self.address, change_pt=False)
+
+    def show_message(self, msg='', style='') -> None:
+        """Уведомление об ошибках или успешном поиске в статус-меню"""
+
+        self.statusBar().showMessage(msg)
+        self.statusBar().setStyleSheet(style)
+
+    def reset_search(self) -> None:
+        """Сброс поиска"""
+
+        self.pt, self.address = None, None
+        self.show_message()
+        self.update_pixmap()
 
     def keyPressEvent(self, event):
         # изменение масштаба
-        if event.key() == Qt.Key_Equal:
+        if event.key() == Qt.Key_PageUp:
             self.scale = min(self.scale + 1, 17)
-        elif event.key() == Qt.Key_Minus:
+        elif event.key() == Qt.Key_PageDown:
             self.scale = max(self.scale - 1, 0)
         # перемещение центра карты
         elif event.key() == Qt.Key_Up:
@@ -157,33 +223,26 @@ class Window(QMainWindow):
         self.update_pixmap()
 
     def mousePressEvent(self, event):
-        l_x, l_y = self.image.x(), self.image.y()
+        if event.button() == Qt.MiddleButton:
+            return
+        l_x, l_y = self.image.x() + 1, self.image.y() + 1
         l_w, l_h = self.image.width(), self.image.height()
-        if l_x <= event.x() <= l_x + self.image.width() and l_y < event.y() < l_y + self.image.height():
-            d_x_px, d_y_px = event.x() - l_x - l_w / 2, -(event.y() - l_y - l_h / 2)
+        if l_x <= event.x() <= l_x + l_w and l_y < event.y() < l_y + l_h:
+            d_x_px, d_y_px = event.x() - l_x - l_w / 2, -(
+                    event.y() - l_y - l_h / 2)
             d_x_degree = 360 / (2 ** self.scale) * d_x_px / 256
             d_y_degree = 180 / (2 ** self.scale) * d_y_px / 256
             coords = f'{self.lon + d_x_degree},{self.lat + d_y_degree}'
-            self.pt = coords + ',pm2rdl'
-            self.search_toponym(geocode=coords, mode=0)
+            if event.button() == Qt.LeftButton:
+                self.pt = coords + ',pm2rdl'
+                self.search_toponym(geocode=coords, change_pt=False)
+            else:
+                self.search_organization(coords)
 
     def closeEvent(self, event):
         """При закрытии формы удаляем файл с картой"""
 
         os.remove(self.map_file)
-
-    def show_message(self, msg='', style='') -> None:
-        """Уведомление об ошибках или успешном поиске в статус-меню"""
-
-        self.statusBar().showMessage(msg)
-        self.statusBar().setStyleSheet(style)
-
-    def reset_search(self) -> None:
-        """Сброс поиска"""
-
-        self.pt, self.address = None, None
-        self.show_message()
-        self.update_pixmap()
 
 
 def except_hook(cls, exception, traceback):
